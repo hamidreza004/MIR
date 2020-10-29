ef31630d163b10f8a2997961076ca4dccb519744
Hamidreza <hamidreza004@gmail.com>
2020-10-29 23:40:47 +0330

    Add preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,14 @@
 import pandas as pd
+from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer, SnowballStemmer
 
 df = pd.read_csv('data/ted_talks.csv')
 ted_talks = df[['description', 'title']]
 
-for index, row in ted_talks.iterrows():
-    row['description'] = row['description'].lower()
-    row['title'] = row['title'].lower()
+print(sent_tokenize("salam khoobi"))
 
-porter = PorterStemmer()
-for stem in ["automatic", "automatically", "swears",
-             "swore", "presidential", "president"]:
-    print(porter.stem(stem))
+for index, row in ted_talks.iterrows():
+    row['description'] = sent_tokenize(row['description'].lower())
+    row['title'] = sent_tokenize(row['title'].lower())
+
+print(ted_talks)
