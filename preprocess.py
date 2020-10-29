525169f3461c747c7524fca5f53afca40b006dfb
Hamidreza <hamidreza004@gmail.com>
2020-10-29 23:43:34 +0330

    Add preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer, SnowballStemmer
+import nltk
+
+nltk.download('punkt')
 
 df = pd.read_csv('data/ted_talks.csv')
 ted_talks = df[['description', 'title']]
