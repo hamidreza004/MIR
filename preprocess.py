f9277f2f854c78662893a86a8fca990a24886834
Hamidreza <hamidreza004@gmail.com>
2020-10-30 01:02:06 +0330

    Print stop words

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,6 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from nltk import WordNetLemmatizer
 
@@ -17,7 +16,6 @@ lemma = WordNetLemmatizer()
 def clean_raw(raw):
     tokens = word_tokenize(raw.lower())
     tokens = [word for word in tokens if word.isalpha()]
-    tokens = [word for word in tokens if not word in stopwords.words("english")]
     tokens = [snowball.stem(word) for word in tokens]
     tokens = [lemma.lemmatize(word, pos="v") for word in tokens]
     tokens = [lemma.lemmatize(word, pos="n") for word in tokens]
@@ -31,12 +29,25 @@ def read_ted_file(path):
     return ted_df
 
 
-def clean_file(df):
+def prepare_text(df):
+    all_tokens_dic = {}
     for index, row in df.iterrows():
         row['description'] = clean_raw(row['description'])
         row['title'] = clean_raw(row['title'])
+        for col_name in ['description', 'title']:
+            for token in row[col_name]:
+                if not token in all_tokens_dic:
+                    all_tokens_dic[token] = 1
+                else:
+                    all_tokens_dic[token] += 1
+    i = 0
+    for token, count in sorted(all_tokens_dic.items(), key=lambda item: item[1], reverse=True):
+        i += 1
+        print(token, count)
+        if i == 100:
+            break
     return df
 
 
-ted_talks = clean_file(read_ted_file('data/ted_talks.csv'))
+ted_talks = prepare_text(read_ted_file('data/ted_talks.csv'))
 print(ted_talks)
