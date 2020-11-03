76413a5019774d5e5aebd8c1a2c5c2dbcf789267
Hamidreza <hamidreza004@gmail.com>
2020-11-04 01:43:38 +0330

    Complete XML to dataframe

--- helper.py
+++ helper.py
@@ -3,6 +3,22 @@ import pandas as pd
 
 
 def XML_to_dataframe(filename):
+    df = pd.DataFrame(columns=['description', 'title'])
     tree = ET.parse(filename)
     root = tree.getroot()
-    print(root[0].attrib)
+    i = 0
+    for elem in root:
+        if elem.tag.endswith("page"):
+            description = ''
+            title = ''
+            for sub_elem in elem:
+                if sub_elem.tag.endswith("title"):
+                    title = sub_elem.text
+                if sub_elem.tag.endswith("revision"):
+                    for sub_sub_elem in sub_elem:
+                        if sub_sub_elem.tag.endswith("text"):
+                            description = sub_sub_elem.text
+            df.loc[i] = [description, title]
+            i += 1
+    print(df)
+    return df
