a13de497c8a79f6acda447f87d3b3cd84f97f8ae
rmool <armin2saadat@gmail.com>
2020-12-18 02:00:22 +0330

    add test and evaluation methods to Random Forest

--- classifiers/random_forest.py
+++ classifiers/random_forest.py
@@ -31,6 +31,41 @@ class RandomForest:
                 x.append(0)
         return self.clf.predict([x])
 
+    def test(self, test_targets, test_tfIdf):
+        self.tp = 0
+        self.tn = 0
+        self.fp = 0
+        self.fn = 0
+
+        predicted = []
+        for doc_tfIdf in test_tfIdf:
+            predicted.append(self.predict(doc_tfIdf))
+        for i, target in enumerate(test_targets):
+            if target == 1:
+                if predicted[i] == 1:
+                    self.tp += 1
+                else:
+                    self.fn += 1
+            else:
+                if predicted[i] == 1:
+                    self.fp += 1
+                else:
+                    self.tn += 1
+
+    def get_accuracy(self):
+        return (self.tp + self.tn) / (self.tp + self.tn + self.fp + self.fn)
+
+    def get_precision(self):
+        return self.tp / (self.tp + self.fp)
+
+    def get_recall(self):
+        return self.tp / (self.tp + self.fn)
+
+    def get_F1(self):
+        P = self.get_precision()
+        R = self.get_recall()
+        return (2 * P * R) / (P + R)
+
 
 target_ = [1, -1]
 vocab = ["good", "bad", "girl", "boy"]
