3a1e2dae192e1cf4143b1ed8ebbc016044ba141e
rmool <armin2saadat@gmail.com>
2020-12-18 01:51:15 +0330

    add train and predict to RandomForest class

--- classifiers/random_forest.py
+++ classifiers/random_forest.py
@@ -1,10 +1,33 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
 
-X, y = make_classification(n_samples=1000, n_features=4,
-                           n_informative=2, n_redundant=0,
-                           random_state=0, shuffle=False)
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(X, y)
 
-print(clf.predict([[0, 0, 0, 0]]))
+class RandomForest:
+    def __init__(self):
+        self.clf = None
+        self.tp = 0
+        self.tn = 0
+        self.fp = 0
+        self.fn = 0
+
+    def train(self, train_targets, train_vocab, train_tfIdf):
+        X = []
+        for doc in train_tfIdf:
+            x = []
+            for term in train_vocab:
+                x.append(doc.get(term))
+            X.append(x)
+        y = train_targets
+        self.clf = RandomForestClassifier(max_depth=2, random_state=0)
+        self.clf.fit(X, y)
+
+    def predict(self, doc_tfIdf):
+        return self.clf.predict([doc_tfIdf])
+
+
+target_ = [1, -1]
+vocab = ["good", "bad", "girl", "boy"]
+tfIdf = [{"good": 4, "bad": 1, "girl": 2, "boy": 2}, {"good": 1, "bad": 4, "girl": 5, "boy": 2}]
+
+rf = RandomForest()
+rf.train(target_, vocab, tfIdf)
+print(rf.predict([1, 4, 5, 2]))
