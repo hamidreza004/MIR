759ed4757be9702aba9c68fa510a8aa3dd7e93d2
rmool <armin2saadat@gmail.com>
2021-02-06 03:04:09 +0330

    add evaluate to gmm

--- clusterings/gmm.py
+++ clusterings/gmm.py
@@ -2,17 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.mixture import GaussianMixture
 from sklearn import metrics
-
-
-# tf_idf = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-# w2v = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-# tags = np.array([0, 1, 1, 2, 0, 1])
+from sklearn.metrics.cluster import contingency_matrix
 
 
 def get_result_df(tf_idf, w2v, tags):
     vectors = ['tf_idf', 'w2v']
     n_components = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    # n_components = [2, 3, 4]
     covariance_types = ['full', 'tied', 'diag', 'spherical']
     max_iters = [100, 200]
 
@@ -30,12 +25,52 @@ def get_result_df(tf_idf, w2v, tags):
     return pd.DataFrame(data=result, columns=['vector', 'n_components', 'covariance_type', 'max_iter', 'AMI'])
 
 
-def get_AMI(vector, tags, n_component, covariance_type, max_iter):
-    gmm = GaussianMixture(n_components=n_component, covariance_type=covariance_type, max_iter=max_iter,
-                          random_state=12).fit(vector)
+def get_evaluation_dataframe(tf_idf, tf_param, w2v, w2v_param, tags):
+    Purity_tf, AMI_tf, NMI_tf, ARI_tf = evaluate(tf_idf, tags, tf_param['n_components'], tf_param['covariance_type'],
+                                                 tf_param['max_iter'])
+    Purity_w2v, AMI_w2v, NMI_w2v, ARI_w2v = evaluate(w2v, tags, w2v_param['n_components'], w2v_param['covariance_type'],
+                                                     w2v_param['max_iter'])
 
+    return (pd.DataFrame({'Purity': [Purity_tf, Purity_w2v], 'Adjusted Mutual Info': [AMI_tf, AMI_w2v],
+                          'Normalized Mutual Info': [NMI_tf, NMI_w2v], 'Adjusted Rand Index': [ARI_tf, ARI_w2v]},
+                         index=['tf_idf', 'w2v']))
+
+
+def get_AMI(vector, tags, n_component, covariance_type, max_iter, random_state=12):
+    gmm = GaussianMixture(n_components=n_component, covariance_type=covariance_type, max_iter=max_iter,
+                          random_state=random_state).fit(vector)
     labels_true = tags
     labels_pred = gmm.predict(vector)
     AMI = round((metrics.cluster.adjusted_mutual_info_score(labels_true, labels_pred)), 6)
 
     return AMI
+
+
+def get_best_randomeness(vector, tags, n_component, covariance_type, max_iter):
+    best_score = 0
+    best_random = 0
+    for i in range(5):
+        score = get_AMI(vector, tags, n_component, covariance_type, max_iter, i * 53)
+        if score > best_score:
+            best_score = score
+            best_random = i * 53
+    return best_random
+
+
+def evaluate(vector, tags, n_component, covariance_type, max_iter):
+    best_random_state = get_best_randomeness(vector, tags, n_component, covariance_type, max_iter)
+    gmm = GaussianMixture(n_components=n_component, covariance_type=covariance_type, max_iter=max_iter,
+                          random_state=best_random_state).fit(vector)
+    labels_true = tags
+    labels_pred = gmm.predict(vector)
+    Purity = round(purity_score(labels_true, labels_pred), 6)
+    AMI = round((metrics.cluster.adjusted_mutual_info_score(labels_true, labels_pred)), 6)
+    NMI = round((metrics.cluster.normalized_mutual_info_score(labels_true, labels_pred)), 6)
+    ARI = round(metrics.cluster.adjusted_rand_score(labels_true, labels_pred), 6)
+
+    return Purity, AMI, NMI, ARI
+
+
+def purity_score(labels_true, labels_pred):
+    matrix = contingency_matrix(labels_true, labels_pred)
+    return np.sum(np.amax(matrix, axis=0)) / np.sum(matrix)
